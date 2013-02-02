250647a26d123f2dd0b4da6cd6de3f0b79758a21
Habli <hablics@math.wisc.edu>
2013-02-02 14:45:20 -0600

    Update ModsymFamiliesElement.py
    Minor change

--- ModsymFamiliesElement.py
+++ ModsymFamiliesElement.py
@@ -12,3 +12,23 @@ class ModsymFamiliesElement(Element):
 		v=[self.data[r].change_d(new_d) for r in range(self.ngens())]
         return ModsymFamiliesElement(self.level,v,self.manin)
     
+	def specialize(self,k):
+		v=[]
+		for j in range(0,len(self.data)):
+			v=v+[self.data[j].specialize(k)]
+		return modsym_dist_aws(self.level,v,self.manin)
+		"""Should we normalzie, so we get a modsym_dist?"""
+	
+
+	def normalize(self):
+		v=[]
+		for j in range(0,len(self.data)):
+			v=v+[self.data[j].normalize()]
+
+	def vector_of_total_measures(self):
+		"""returns the vector comprising of the total measure of each distribution defining Phi"""
+		v=[]
+		for j in range(0,self.ngens()):
+			v=v+[self.data[j].moments[0]]
+		return v
+		"""I am not sure it should be here"
